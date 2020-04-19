8b17d2f91ef86df2b21179af4ad926f29e7be71f
sovathapann <sovathapann@gmail.com>
2020-04-20 00:03:35 +0700

    Version 1.0

---
+++ app/src/main/java/android/example/com/researchproject/ui/home/HomeViewModel.java
@@ -0,0 +1,31 @@
+package android.example.com.researchproject.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
